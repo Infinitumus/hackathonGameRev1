a75460c40d5fcbf235ef1e40643d36806c8c77e5
Infinitumus <89930093+Infinitumus@users.noreply.github.com>
2022-06-24 18:54:50 +0700

    delete game dir

--- src/game/Test.java
+++
@@ -1,9 +0,0 @@
-package game;
-
-public class Test {
-    public static void main(String[] args) {
-        System.out.println("Hello");
-        System.out.println("Hello");
-        System.out.println("Hello");
-    }
-}
